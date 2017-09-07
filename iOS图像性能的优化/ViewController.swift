e103f67b6d1e452f3048cd8f7d4767553e930976
曹檑 <1090491594@qq.com>
2017-09-07 11:22:46 +0800

    Initial Commit

---
+++ iOS图像性能的优化/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS图像性能的优化
+//
+//  Created by Zeus on 2017/9/7.
+//  Copyright © 2017年 Zeus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ iOS图像性能的优化Tests/iOS_______Tests.swift
@@ -0,0 +1,36 @@
+//
+//  iOS_______Tests.swift
+//  iOS图像性能的优化Tests
+//
+//  Created by Zeus on 2017/9/7.
+//  Copyright © 2017年 Zeus. All rights reserved.
+//
+
+import XCTest
+@testable import iOS_______
+
+class iOS_______Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
